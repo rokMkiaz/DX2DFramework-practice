dcb95e9157b240e6bcf4002319f5dc1ecd19f7e7
남일우 <93506849+rokMkiaz@users.noreply.github.com>
2021-12-03 11:43:33 +0900

    Add files via upload

--- SNEngine_2D/Utility/Geometry_Generator.cpp
+++ SNEngine_2D/Utility/Geometry_Generator.cpp
@@ -1,5 +1,14 @@
 #include "stdafx.h"
 #include"Geometry_Generator.h"
+void Geometry_Generator::CreateQuad(D3D11_Geometry<struct D3D11_VertexColor>& geometry, const D3DXCOLOR& color)
+{
+	geometry.AddVertex(D3D11_VertexColor(D3DXVECTOR3(-0.5f, -0.5f, 0.0f),color));
+	geometry.AddVertex(D3D11_VertexColor(D3DXVECTOR3(-0.5f, +0.5f, 0.0f),color));
+	geometry.AddVertex(D3D11_VertexColor(D3DXVECTOR3(+0.5f, -0.5f, 0.0f),color));
+	geometry.AddVertex(D3D11_VertexColor(D3DXVECTOR3(+0.5f, +0.5f, 0.0f),color));
+	geometry.AddIndex(0);	geometry.AddIndex(1);	geometry.AddIndex(2);
+	geometry.AddIndex(2);	geometry.AddIndex(1);	geometry.AddIndex(3);
+}
 
 void Geometry_Generator::CreateQuad(D3D11_Geometry<struct D3D11_VertexTexture>& geometry)
 {
--- SNEngine_2D/Utility/Geometry_Generator.h
+++ SNEngine_2D/Utility/Geometry_Generator.h
@@ -5,6 +5,7 @@ template<typename T> class D3D11_Geometry;
 class Geometry_Generator final
 {
 public:
+	static void CreateQuad(D3D11_Geometry<struct D3D11_VertexColor>& geometry, const D3DXCOLOR & color);
 	static void CreateQuad(D3D11_Geometry<struct D3D11_VertexTexture>& geometry);
 
 };
