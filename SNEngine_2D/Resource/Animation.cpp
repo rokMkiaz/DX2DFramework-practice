860261cce73740ca2f4bb3327827a0bed738fe98
남일우 <93506849+rokMkiaz@users.noreply.github.com>
2021-12-11 17:52:28 +0900

    Add files via upload

--- SNEngine_2D/Resource/Animation.cpp
+++ SNEngine_2D/Resource/Animation.cpp
@@ -1,8 +1,8 @@
-#include"stdafx.h"
-#include"Animation.h"
+#include "stdafx.h"
+#include "Animation.h"
 
 Animation::Animation(Context* const context)
-	:context(context)
+	: context(context)
 {
 }
 
@@ -12,25 +12,88 @@ Animation::~Animation()
 	keyframes.shrink_to_fit();
 }
 
+bool Animation::SaveToFile(const std::string& path)
+{
+	Xml::XMLDocument doc;
+
+	Xml::XMLDeclaration* decl = doc.NewDeclaration();
+	doc.LinkEndChild(decl);
+
+	Xml::XMLElement* root = doc.NewElement("Animation");
+	doc.LinkEndChild(root);
+
+	root->SetAttribute("Name", animation_name.c_str());
+	root->SetAttribute("Type", static_cast<uint>(repeat_type));
+	root->SetAttribute("TextureSizeX", sprite_texture_size.x);
+	root->SetAttribute("TextureSizeY", sprite_texture_size.y);
+
+	for (const auto& keyframe : keyframes)
+	{
+		Xml::XMLElement* first_element = doc.NewElement("Keyframe");
+		root->LinkEndChild(first_element);
+
+		first_element->SetAttribute("OffsetX", keyframe.offset.x);
+		first_element->SetAttribute("OffsetY", keyframe.offset.y);
+		first_element->SetAttribute("SizeX", keyframe.size.x);
+		first_element->SetAttribute("SizeY", keyframe.size.y);
+		first_element->SetAttribute("Time", keyframe.time);
+	}
+
+	return Xml::XMLError::XML_SUCCESS == doc.SaveFile(path.c_str());
+}
+bool Animation::LoadFromFile(const std::string& path)
+{
+	Xml::XMLDocument doc;
+	Xml::XMLError error = doc.LoadFile(path.c_str());
+	if (error != Xml::XMLError::XML_SUCCESS)
+	{
+		assert(false);
+		return false;
+	}
+
+	Xml::XMLElement* root = doc.FirstChildElement();
+	animation_name = root->Attribute("Name");
+	repeat_type = static_cast<RepeatType>(root->UnsignedAttribute("Type"));
+	sprite_texture_size.x = root->FloatAttribute("TextureSizeX");
+	sprite_texture_size.y = root->FloatAttribute("TextureSizeY");
+
+	Xml::XMLElement* first_element = root->FirstChildElement();
+	for (; first_element != nullptr; first_element = first_element->NextSiblingElement())
+	{
+		D3DXVECTOR2 offset;
+		offset.x = first_element->FloatAttribute("OffsetX");
+		offset.x = first_element->FloatAttribute("OffsetY");
+
+		D3DXVECTOR2 size;
+		size.x = first_element->FloatAttribute("SizeX");
+		size.x = first_element->FloatAttribute("SizeY");
+
+		float time = first_element->FloatAttribute("Time");
+
+		AddKeyframe(offset, size, time);
+	}
+
+	return true;
+}
+
 void Animation::SetSpriteTexture(const std::string& path)
 {
 	sprite_texture = std::make_shared<D3D11_Texture>(context->GetSubsystem<Graphics>());
 	sprite_texture->Create(path);
 }
 
-auto Animation::GetKeyFrameFromIndex(const uint& index) -> const Keyframe* const
+auto Animation::GetKeyframeFromIndex(const uint& index) -> const Keyframe* const
 {
 	assert(index < keyframes.size());
-
 	return &keyframes[index];
 }
 
-void Animation::AddKeyFrame(const Keyframe& keyframe)
+void Animation::AddKeyframe(const Keyframe& keyframe)
 {
 	keyframes.emplace_back(keyframe);
 }
 
-void Animation::AddKeyFrame(const D3DXVECTOR2& offset, const D3DXVECTOR2& size, const double& time)
+void Animation::AddKeyframe(const D3DXVECTOR2& offset, const D3DXVECTOR2& size, const double& time)
 {
 	keyframes.emplace_back(offset, size, time);
 }
--- SNEngine_2D/Resource/Animation.h
+++ SNEngine_2D/Resource/Animation.h
@@ -1,24 +1,35 @@
 #pragma once
-enum class RepeatType :uint
+
+enum class RepeatType : uint
 {
-	Once,Loop,
+	Once, Loop,
 };
+
 struct Keyframe final
 {
 	Keyframe() = default;
 	Keyframe(const D3DXVECTOR2& offset, const D3DXVECTOR2& size, const double& time)
-		: offset(offset), size(size), time(time){}
+		: offset(offset)
+		, size(size)
+		, time(time)
+	{}
 
 	D3DXVECTOR2 offset{ 0.0f, 0.0f };
-	D3DXVECTOR2 size{0.0f , 0.0f };
+	D3DXVECTOR2 size{ 0.0f, 0.0f };
 	double time{ 0.0 };
 };
 
 class Animation final
 {
 public:
-	Animation(class Context*const context);
+	Animation(class Context* const context);
 	~Animation();
+
+	bool SaveToFile(const std::string& path);
+	bool LoadFromFile(const std::string& path);
+
+	auto GetAnimationName() const ->const std::string& { return animation_name; }
+	void SetAnimationName(const std::string& name) { this->animation_name = name; }
 
 	auto GetRepeatType() const -> const RepeatType& { return repeat_type; }
 	void SetRepeatType(const RepeatType& repeat_type) { this->repeat_type = repeat_type; }
@@ -30,19 +41,21 @@ public:
 	void SetSpriteTexture(const std::shared_ptr<class D3D11_Texture>& sprite_texture) { this->sprite_texture = sprite_texture; }
 	void SetSpriteTexture(const std::string& path);
 
-	auto GetKeyFrames() const -> const std::vector<Keyframe>& { return keyframes; }
-	void SetKeyFrames(const std::vector<Keyframe>& keyframes) { this->keyframes = keyframes; }
+	auto GetKeyframes() const -> const std::vector<Keyframe>& { return keyframes; }
+	void SetKeyframes(const std::vector<Keyframe>& keyframes) { this->keyframes = keyframes; }
 
-	auto GetKeyFrameFromIndex(const uint& index) -> const Keyframe* const;
-	auto GetKeyFrameCount() const -> const uint { return keyframes.size(); }
+	auto GetKeyframeFromIndex(const uint& index) -> const Keyframe* const;
+	auto GetKeyframeCount() const -> const uint { return keyframes.size(); }
 
-	void AddKeyFrame(const Keyframe& keyframe);
-	void AddKeyFrame(const D3DXVECTOR2& offset, const D3DXVECTOR2& size, const double& time);
-		 
+	void AddKeyframe(const Keyframe& keyframe);
+	void AddKeyframe(const D3DXVECTOR2& offset, const D3DXVECTOR2& size, const double& time);
+
 private:
 	class Context* context = nullptr;
 	RepeatType repeat_type = RepeatType::Loop;
 	D3DXVECTOR2 sprite_texture_size = D3DXVECTOR2(1.0f, 1.0f);
+
+	std::string animation_name = "";
 	std::shared_ptr<class D3D11_Texture> sprite_texture;
 	std::vector<Keyframe> keyframes;
 };
