16ee55f319c515ce304d66a0bd51705ec0fd5708
남일우 <93506849+rokMkiaz@users.noreply.github.com>
2021-12-26 16:37:13 +0900

    Add files via upload

---
+++ SNCreate_3D/Framework/Renders/Buffers.h
@@ -0,0 +1,64 @@
+#pragma once
+
+class VertexBuffer //인터페이스를 상속받아쓰는게 정석임 Wrapper Class 
+{
+public:
+	VertexBuffer(void* data, UINT count, UINT stride, UINT slot = 0, bool bCpuWrite = false, bool bGpuWrite = false);
+	~VertexBuffer();
+
+	UINT Count() { return count; }
+	UINT Stride() { return stride; }
+	ID3D11Buffer* Buffer() { return buffer; }
+
+	void Render();
+
+private:
+	ID3D11Buffer* buffer;
+
+	void* data; //정점정보 받아오는것
+	UINT count;
+	UINT stride;
+	UINT slot;
+
+	bool bCpuWrite;
+	bool bGpuWrite;
+};
+
+/// /////////////////////////////////////////////////////////////////////////
+class IndexBuffer
+{
+public:
+	IndexBuffer(void* data, UINT count);
+	~IndexBuffer();
+
+	UINT Count() { return count; }
+	ID3D11Buffer* Buffer() { return buffer; }
+
+	void Render();
+
+private:
+	ID3D11Buffer* buffer;
+
+	void* data;
+	UINT count;
+};
+
+/// /////////////////////////////////////////////////////////////////////////
+
+class ConstantBuffer
+{
+public:
+	ConstantBuffer(void* data, UINT dataSize);
+	~ConstantBuffer();
+
+	ID3D11Buffer* Buffer() { return buffer; }
+
+	void Render();
+
+private:
+	ID3D11Buffer* buffer;
+
+	void* data ;
+	UINT dataSize ;
+
+};
