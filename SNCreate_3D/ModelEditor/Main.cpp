a9934d650d4dfe0e7c985b9e91a916f13ff9204a
남일우 <93506849+rokMkiaz@users.noreply.github.com>
2021-12-27 17:37:33 +0900

    Add files via upload

---
+++ SNCreate_3D/ModelEditor/Main.cpp
@@ -0,0 +1,115 @@
+#include "stdafx.h"
+#include "Main.h"
+#include "Systems/Window.h"
+
+#include"World\GridDemo.h"
+#include"World\WorldDemo3.h"
+#include"World/RotationDemo.h"
+#include"Vertex/UserInterfaceDemo.h"
+#include"Vertex/RectDemo.h"
+#include"Vertex/Vertex_Line.h"
+#include"Vertex/Vertex_Line2.h"
+#include"Vertex/TriangleList.h"
+#include"Mesh/CubeDemo.h"
+#include"Mesh/MeshDemo.h"
+#include"Texture/TextureDemo.h"
+#include"Texture/TextureLoadDemo.h"
+#include"Texture/TextureSamplerDemo.h"
+#include"Terrain/HeightMapDemo.h"
+#include"Terrain/NormalVectorDemo.h"
+#include"Terrain/GetHeightDemo.h"
+#include"Terrain/GetVerticalRaycast.h"
+#include"Objects/CubeMapDemo.h"
+#include"Objects/CubeSkyDemo.h"
+
+void Main::Initialize()
+{
+	Push(new CubeSkyDemo);
+	//Push(new CubeMapDemo);
+	//Push(new MeshDemo);
+	//Push(new GetVerticalRaycast);
+	//Push(new GetHeightDemo);
+	//Push(new NormalVectorDemo);
+	//Push(new HeightMapDemo);
+	//Push(new TextureSamplerDemo);
+	//Push(new TextureLoadDemo);
+	//Push(new TextureDemo);
+	//Push(new RotationDemo);
+	//Push(new  GridDemo);
+	//Push(new CubeDemo);
+	//Push(new UserInterfaceDemo);
+	//Push(new RectDemo());
+	//Push(new TriangleList());
+}
+
+void Main::Ready()
+{
+
+}
+
+void Main::Destroy()
+{
+	for (IExecute* exe : executes)
+	{
+		exe->Destroy();
+		SafeDelete(exe);
+	}
+}
+
+void Main::Update()
+{
+	for (IExecute* exe : executes)
+		exe->Update();
+}
+
+void Main::PreRender()
+{
+	for (IExecute* exe : executes)
+		exe->PreRender();
+}
+
+void Main::Render()
+{
+	for (IExecute* exe : executes)
+		exe->Render();
+}
+
+void Main::PostRender()
+{
+	for (IExecute* exe : executes)
+		exe->PostRender();
+}
+
+void Main::ResizeScreen()
+{
+	for (IExecute* exe : executes)
+		exe->ResizeScreen();
+}
+
+void Main::Push(IExecute * execute)
+{
+	executes.push_back(execute);
+
+	execute->Initialize();
+}
+
+int WINAPI WinMain(HINSTANCE instance, HINSTANCE prevInstance, LPSTR param, int command)
+{
+	D3DDesc desc;
+	desc.AppName = L"D3D Game";
+	desc.Instance = instance;
+	desc.bFullScreen = false;
+	desc.bVsync = false;
+	desc.Handle = NULL;
+	desc.Width = 1280;
+	desc.Height = 720;
+	desc.Background = Color(0.3f, 0.3f, 0.3f, 1.0f);
+	D3D::SetDesc(desc);
+
+	Main* main = new Main();
+	WPARAM wParam = Window::Run(main);
+
+	SafeDelete(main);
+
+	return wParam;
+}
