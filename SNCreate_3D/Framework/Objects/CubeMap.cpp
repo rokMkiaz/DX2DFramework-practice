1f699805544095a27646b7de2b558c3695ad49e5
남일우 <93506849+rokMkiaz@users.noreply.github.com>
2021-12-27 17:38:15 +0900

    Add files via upload

--- SNCreate_3D/Framework/Objects/CubeMap.cpp
+++ SNCreate_3D/Framework/Objects/CubeMap.cpp
@@ -4,14 +4,14 @@
 CubeMap::CubeMap(Shader* shader)
 	:shader(shader)
 {
-	cube = new MeshSphere(shader, 0.5f);
+	mesh = new MeshSphere(shader, 0.5f);
 
 	sSrv = shader->AsSRV("CubeMap");
 }
 
 CubeMap::~CubeMap()
 {
-	SafeDelete(cube);
+	SafeDelete(mesh);
 
 	SafeRelease(srv);
 }
@@ -29,72 +29,13 @@ void CubeMap::Texture(wstring file)
 
 void CubeMap::Update()
 {
-	cube->Update();
+	mesh->Update();
 }
 
 void CubeMap::Render()
 {
 	sSrv->SetResource(srv);
 
-	cube->Render();
+	mesh->Render();
 }
 
-void CubeMap::Position(float x, float y, float z)
-{
-	cube->Position(x, y, z);
-}
-
-void CubeMap::Position(Vector3& vec)
-{
-	cube->Position(vec);
-}
-
-void CubeMap::Position(Vector3* vec)
-{
-	cube->Position(vec);
-}
-
-void CubeMap::Rotation(float x, float y, float z)
-{
-	cube->Rotation(x, y, z);
-}
-
-void CubeMap::Rotation(Vector3& vec)
-{
-	cube->Rotation(vec);
-}
-
-void CubeMap::Rotation(Vector3* vec)
-{
-	cube->Rotation(vec);
-}
-
-void CubeMap::RotationDegree(float x, float y, float z)
-{
-	cube->RotationDegree(x, y, z);
-}
-
-void CubeMap::RotationDegree(Vector3& vec)
-{
-	cube->RotationDegree(vec);
-}
-
-void CubeMap::RotationDegree(Vector3* vec)
-{
-	cube->RotationDegree(vec);
-}
-
-void CubeMap::Scale(float x, float y, float z)
-{
-	cube->Scale(x, y, z);
-}
-
-void CubeMap::Scale(Vector3& vec)
-{
-	cube->Scale(vec);
-}
-
-void CubeMap::Scale(Vector3* vec)
-{
-	cube->Scale(vec);
-}
