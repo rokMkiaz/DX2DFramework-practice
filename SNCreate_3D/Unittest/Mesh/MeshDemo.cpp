abd8c1fad9941e6d8eb773451fc4546fb1a17708
남일우 <93506849+rokMkiaz@users.noreply.github.com>
2021-12-27 17:43:24 +0900

    Add files via upload

--- SNCreate_3D/Unittest/Mesh/MeshDemo.cpp
+++ SNCreate_3D/Unittest/Mesh/MeshDemo.cpp
@@ -80,36 +80,36 @@ void MeshDemo::Render()
 void MeshDemo::CreateMesh()
 {
 	cube = new MeshCube(shader);
-	cube->Position(0, 5, 0);
-	cube->Scale(20, 10, 20);
+	cube->GetTransform()->Position(0, 5, 0);
+	cube->GetTransform()->Scale(20, 10, 20);
 	cube->DiffuseMap(L"Stones.png");
 
 	grid = new MeshGrid(shader, 6, 6);
-	grid->Scale(12, 1, 12);
+	grid->GetTransform()->Scale(12, 1, 12);
 	grid->DiffuseMap(L"Floor.png");
 
 
 	for (UINT i = 0; i < 5; i++)
 	{
 		cylinder[i * 2] = new MeshCylinder(shader, 0.5f, 3.0f, 20, 20);
-		cylinder[i * 2]->Position(-30, 6, (float)i * 15.0f - 15.0f);
-		cylinder[i * 2]->Scale(5, 5, 5);
+		cylinder[i * 2]->GetTransform()->Position(-30, 6, (float)i * 15.0f - 15.0f);
+		cylinder[i * 2]->GetTransform()->Scale(5, 5, 5);
 		cylinder[i * 2]->DiffuseMap(L"Bricks.png");
 
 		cylinder[i * 2 + 1] = new MeshCylinder(shader, 0.5f, 3.0f, 20, 20);
-		cylinder[i * 2 + 1]->Position(30, 6, (float)i * 15.0f - 15.0f);
-		cylinder[i * 2 + 1]->Scale(5, 5, 5);
+		cylinder[i * 2 + 1]->GetTransform()->Position(30, 6, (float)i * 15.0f - 15.0f);
+		cylinder[i * 2 + 1]->GetTransform()->Scale(5, 5, 5);
 		cylinder[i * 2 + 1]->DiffuseMap(L"Bricks.png");
 
 
 		sphere[i * 2] = new MeshSphere(shader, 0.5f, 20, 20);
-		sphere[i * 2]->Position(-30, 15.5f, (float)i * 15.0f - 15.0f);
-		sphere[i * 2]->Scale(5, 5, 5);
+		sphere[i * 2]->GetTransform()->Position(-30, 15.5f, (float)i * 15.0f - 15.0f);
+		sphere[i * 2]->GetTransform()->Scale(5, 5, 5);
 		sphere[i * 2]->DiffuseMap(L"Wall.png");
 
 		sphere[i * 2 + 1] = new MeshSphere(shader, 0.5f, 20, 20);
-		sphere[i * 2 + 1]->Position(30, 15.5f, (float)i * 15.0f - 15.0f);
-		sphere[i * 2 + 1]->Scale(5, 5, 5);
+		sphere[i * 2 + 1]->GetTransform()->Position(30, 15.5f, (float)i * 15.0f - 15.0f);
+		sphere[i * 2 + 1]->GetTransform()->Scale(5, 5, 5);
 		sphere[i * 2 + 1]->DiffuseMap(L"Wall.png");
 	}
 }
